76d3eeb23bce9b4c7d2b3229ca7bbd935728cef3
GypoTalamus <vidzhet@inbox.ru>
2022-03-16 23:30:05 +0700

    Com 003: Added DoWhile for Menu & RandomGenerator

--- MasterMind_v2.cpp
+++ MasterMind_v2.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-//int[] StartNumber;  // загадываемое число
-//int[] Otgadka;  // Ваш ответ
+int *StartNumber;  // загадываемое число
+int *Otgadka;  // Ваш ответ
 int Counter = 1; //Счётчик ответов
 int Score; //Количество очков сложности
 int Level; //Сложность игры
@@ -17,26 +17,62 @@ int Bulls = 0;
 int Cows = 0;
 
 
+
+// Генератор множества Nq не повторяющихся целых чисел в диапазоне от 0 до Nm-1 (RVA)
+static void GenSetN(int Nq, int Nm, int *qN)
+{
+    /*srand(time(0));*/
+    //Random r = new Random();
+    int p, k = 0;
+    while (k < Nq)
+    {
+        //p = r.Next(Nm);
+        p = rand() % Nm;
+        bool b = true;
+        for (int i = 0; i < k; i++)
+            if (p == qN[i])
+            {
+                b = false;
+                break;
+            }
+        if (b)
+        {
+            qN[k] = p;
+            k++;
+        }
+    }
+}
+
 void Menu(int ch)
 {
     switch (ch)
     {
     case 1:
         cout << "Компьютер загадал 4-значное число с НЕповторяющимися цифрами. У Вас есть 10 попыток." << endl;
-        //sum();
+        Level = 4;
+        Score = 10;
+        ScorePoint = 10;
+        StartNumber = new int[Level];
+        Otgadka = new int[Level];
+        GenSetN(Level, 10, StartNumber);
+        cout << "Zagadano: ";
+        for (int i = 0; i < Level; i++)
+        cout << StartNumber[i];
+        cout << endl;
+
         break;
-    case 2:
-        cout << "Обычная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
-        //system("Pause");
-        break;
-    case 3:
-        cout << "Сложная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
-        //system("Pause");
-        break;
-    case 4:
-        cout << "Турнирная таблица временно неджоступна. Компьютер не может выполнить Ваш запрос." << endl;
-        //system("Pause");
-        break;
+    //case 2:
+    //    cout << "Обычная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
+    //    //
+    //    break;
+    //case 3:
+    //    cout << "Сложная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
+    //    //
+    //    break;
+    //case 4:
+    //    cout << "Турнирная таблица временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
+    //    //
+    //    break;
     case 0:
         cout << "Прощай, Игрок! Надеюсь, мы ещё встретимся!.." << endl;
         break;
@@ -49,7 +85,10 @@ void Menu(int ch)
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl <<
+
+    srand(time(0));
+
+    /*cout << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl <<
         " = = = =  Меню  = = = = " << endl << endl <<
         " (!) Пункты со * временно недоступны..." << endl << endl <<
         "1. Легкая игра (4-значное число, 10 попыток)." << endl <<
@@ -57,21 +96,36 @@ int main()
         "3)* Сложная игра(10 - значное число, 40 попыток)." << endl <<
         "4)* Турнирная таблица." << endl <<
         "0) Выход." << endl << 
-        "Ваш выбор: ";
+        "Ваш выбор: ";*/
+
+
 
     int choice;
-    cin >> choice;
-    Menu(choice);
+    
+    //Цикл вывода меню повторяется, пока игрок не введет 0 - выход из игры
+    do
+    {
+        cout << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl <<
+            " = = = =  Меню  = = = = " << endl << endl <<
+            " (!) Пункты со * временно недоступны..." << endl << endl <<
+            "1. Легкая игра (4-значное число, 10 попыток)." << endl <<
+            "2)* Обычная игра(6 - значное число, 20 попыток)." << endl <<
+            "3)* Сложная игра(10 - значное число, 40 попыток)." << endl <<
+            "4)* Турнирная таблица." << endl <<
+            "0) Выход." << endl <<
+            "Ваш выбор: ";
+        cin >> choice;
+        Menu(choice);
+    } while (choice != 0);
+    
+    //int* StartNumber = new int[Level];
+    //int* Otgadka = new int[Level];
 
      system("Pause");
 }
 
 
 /*
-namespace MasterMind
-{
-    public partial class Form1 : Form
-    {
         int[] StartNumber;  // загадываемое число
         int[] Otgadka;  // Ваш ответ
         int Counter = 1; //Счётчик ответов
@@ -82,38 +136,6 @@ namespace MasterMind
         int Bulls = 0;
         int Cows = 0;
 
-        public Form1()
-        {
-            InitializeComponent();
-            panelLevelSelect.Visible = true;
-            panel2.Location = new Point(0, 20);
-            panel1.Location = new Point(400, 20);
-            panelLevelSelect.Location = new Point(15,60);
-        }
-
-        private void buttonLevelEasy_Click(object sender, EventArgs e)
-        {
-            Level = 4;
-            Score = 10;
-            mtbInputOtgadka.Mask = "0000";
-            StartButton();
-        }
-
-        private void buttonLevelMiddle_Click(object sender, EventArgs e)
-        {
-            Level = 6;
-            Score = 20;
-            mtbInputOtgadka.Mask = "000000";
-            StartButton();
-        }
-
-        private void buttonLevelHard_Click(object sender, EventArgs e)
-        {
-            Level = 8;
-            Score = 40;
-            mtbInputOtgadka.Mask = "00000000";
-            StartButton();
-        }
 
         private void buttonPopytka_Click(object sender, EventArgs e) //кнопка Угадать
         {
@@ -125,8 +147,7 @@ namespace MasterMind
             if (ScorePoint > 1)
             {
 
-                Console.WriteLine(mtbInputOtgadka.Text);
-                string InputOtgadka = mtbInputOtgadka.Text;
+                
                 for (int it = 0; it < Level; it++)
                 {
                     char c = InputOtgadka[it];
@@ -171,32 +192,20 @@ namespace MasterMind
             labelScorePoint.Text = ScorePoint + " ";
         }
 
-        private void buttonStart_Click(object sender, EventArgs e)
-        {
-            StartButton();
-        }
+       
 
         //берешь функцию
         //создаёшь mock объект
         //вызываешь эту функцию с  mock объектом и проверяешь каждую строчку
+
         private void StartButton()
         {
-            panelLevelSelect.Visible = false;
-            panel1.Visible = true;
-            panel1.Location = new Point(0, 20);
             StartNumber = new int[Level];
             Otgadka = new int[Level];
-            labelLevel.Text = Level + " ";
-            labelScorePoint.Text = Score + " ";
-            rtbChat.Clear();
-            mtbInputOtgadka.Text = "";
+            
             GenSetN(Level, 10, StartNumber);
             ScorePoint = Score;
-            labelScorePoint.Text = Score + " ";
-            buttonStart.Visible = false;
-            buttonPopytka.Visible = true;
-            labelInputOtgadka.Visible = true;
-            mtbInputOtgadka.Visible = true;
+            
             Counter = 1;
         }
 
