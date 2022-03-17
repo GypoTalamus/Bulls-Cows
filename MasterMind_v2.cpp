1c121011abc7dacfa2153db32c89051a4f865931
GypoTalamus <vidzhet@inbox.ru>
2022-03-18 00:34:44 +0700

    Com 003-002 Many many fixes, no comments

--- MasterMind_v2.cpp
+++ MasterMind_v2.cpp
@@ -13,11 +13,10 @@ int *Otgadka;  // Ваш ответ
 int Counter = 1; //Счётчик ответов
 int Score; //Количество очков сложности
 int Level; //Сложность игры
-int ScorePoint;//Счётчик очков
+//int ScorePoint;//Счётчик очков - не реализована турнирная таблица
 
 int Bulls = 0; //Счётчик Быков
 int Cows = 0; //Счётчик Коров
-
 
 
 // Сравнение на чистое совпадение
@@ -26,7 +25,6 @@ static bool NumberCompare(int * StN, int *Otg)
     bool sovp = true;
     for (int i = 0; i < Level; i++)
     {
-        cout << "StN[" << i << "] = " << StN[i] << ";  Otg[" << i << "] = " << Otg[i] << endl;
         if (StN[i] != Otg[i])
         {
             sovp = false;
@@ -57,7 +55,7 @@ static int BullsAndСows(int *StN, int *Otg)
     return k;
 }
 
-// Генератор множества Nq не повторяющихся целых чисел в диапазоне от 0 до Nm-1 (RVA)
+// Генератор множества не повторяющихся целых чисел в диапазоне от 0 до Maxis-1
 static void GenSetN(int Lvl, int Maxis, int *StN)
 {
     int p, k = 0;
@@ -103,46 +101,15 @@ static bool Answer(int * Otg)
             }
             else
             {
-                cout << "Вы ввели НЕ " << Level << "-хзначное число!" << endl << "Введите Ваше " << Level << "-значное число: " << endl;
-                //bb = false;
-                //return bb;
+                cout << "Игрок, Вы ввели НЕ " << Level << "-хзначное число! Будьте так любезны, введите " << Level << "-значное число: " << endl;
+                break;
             }
         }
     }
 
-    //string yyyy;
-    //char yyyyCH[4];
-    //cout <<  "Введите Ваше 4-значное число: ";
-    //while (bb) {
-    //    cin >> yyyy;
-    //    yyyyCH[0] = yyyy[0];
-    //    yyyyCH[1] = yyyy[1];
-    //    yyyyCH[2] = yyyy[2];
-    //    yyyyCH[3] = yyyy[3];
-    //    //первый и второй третий четвертый элемент строки присвоим char-вой переменной чтобы проверить с помощью isdigit число они или нет
-    //    
-    //    if (yyyy.length() == 4 && isdigit(yyyyCH[0]) && isdigit(yyyyCH[1]) && isdigit(yyyyCH[2]) && isdigit(yyyyCH[3])) 
-    //    {
-    //        bb = false; 
-    //    }
-    //    else
-    //    { 
-    //        cout << "Вы ввели НЕ " << Level << "-хзначное число!" << endl <<  "Введите Ваше 4-значное число: " << endl; 
-    //    }
-    //}
-
-    cout << "Otgadano vot tak: ";
-    for (int i = 0; i < Level; i++)
-    {
-        cout << Otgadka[i];
-    }
-    cout << endl << endl;
     bb = true;
     return bb;
 }
-
-
-
 
 void ProverkaOtveta()
 {
@@ -152,9 +119,8 @@ void ProverkaOtveta()
         if (Answer(Otgadka))  // ответ
         {
             if (NumberCompare(StartNumber, Otgadka))
-            //if (tak)
             {
-                cout << "Вы угадали число c " << Counter << " попытки!";
+                cout << "Вы угадали число c " << Counter << " попытки!" << endl;
                 return;
             }
         }
@@ -170,44 +136,49 @@ void ProverkaOtveta()
         }
         Counter++;
     }
+
+    cout << "Вы не угадали загаданное компьютером число ";
+    for (int i = 0; i < Level; i++)
+        cout << StartNumber[i];
+    cout << " даже за " << Score << " предоставленных попыток..." << endl;
 }
 
 
+void Zagadano()
+{
+    cout << "Загаданное число: ";
+    for (int i = 0; i < Level; i++)
+           cout << StartNumber[i];
+    cout << endl << endl;
+}
 
+void Level_Settings()
+{
+    Counter = 1;
+    //ScorePoint = Score;
+    StartNumber = new int[Level];
+    Otgadka = new int[Level];
+    GenSetN(Level, Score, StartNumber);
+    // Zagadano();
+    ProverkaOtveta();
+    cout << "Игра завершена!" << endl << endl;
+    system("pause");
+
+}
 
 void Level_Easy()
 {
     Level = 4;
     Score = 10;
-    ScorePoint = 10;
-    Counter = 1;
-    StartNumber = new int[Level];
-    Otgadka = new int[Level];
-    GenSetN(Level, 10, StartNumber);
-    /**/
-    cout << "Zagadano: ";
-    for (int i = 0; i < Level; i++)
-    {
-        cout << StartNumber[i];
-    }
-    cout << endl << endl;
-    /**/
-    ProverkaOtveta();
+    Level_Settings();
 }
 
-
-
-
-void Proverb()
+void Level_Medium()
 {
-    string maskue = "0000";
-    string first, second, third;
-    regex rx("[0-9]{4}");
-    cin >> first >> second >> third;
-    cout << endl << setw(4) << regex_match(first, rx) << "; " << setw(4) << regex_match(second, rx) << "; " << setw(4) << regex_match(third, rx) << "; " << endl;
+    Level = 6;
+    Score = 20;
+    Level_Settings();
 }
-
-
 
 
 void Menu(int ch)
@@ -219,21 +190,13 @@ void Menu(int ch)
         Level_Easy();
 
         break;
-    //case 2:
-    //    cout << "Обычная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
-    //    //
-    //    break;
+    case 2:
+        Level_Medium();
+        break;
     //case 3:
     //    cout << "Сложная игра временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
     //    //
     //    break;
-    //case 4:
-    //    cout << "Турнирная таблица временно недоступна. Компьютер не может выполнить Ваш запрос." << endl;
-    //    //
-    //    break;
-    case 5:
-        Proverb();
-        break;
     case 0:
         cout << "Прощай, Игрок! Надеюсь, мы ещё встретимся!.." << endl;
         break;
@@ -249,221 +212,23 @@ int main()
 
     srand(time(0));
 
+    cout << endl << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl << endl;
+
     int choice;
     
     //Цикл вывода меню повторяется, пока игрок не введет 0 - выход из игры
     do
     {
-        cout << endl << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl <<
-            " = = = =  Меню  = = = = " << endl << endl <<
+        Level = 0;
+        Score = 0;
+        cout <<  " = = = =  Меню  = = = = " << endl << endl <<
             " (!) Пункты со * временно недоступны..." << endl << endl <<
-            "1. Легкая игра (4-значное число, 10 попыток)." << endl <<
-            /*"2)* Обычная игра(6 - значное число, 20 попыток)." << endl <<
-            "3)* Сложная игра(10 - значное число, 40 попыток)." << endl <<
-            "4)* Турнирная таблица." << endl <<*/
-            "5) Проверка ввода" << endl <<
+            "1) Легкая игра (4-значное число, 10 попыток)." << endl <<
+            "2) Средняя игра (6 - значное число, 20 попыток)." << endl <<
+            /*"3)* Сложная игра(10 - значное число, 40 попыток)." << endl <<*/
             "0) Выход." << endl <<
             "Ваш выбор: ";
         cin >> choice;
         Menu(choice);
     } while (choice != 0);
-    
-
-     system("Pause");
 }
-
-
-/*
-        int[] StartNumber;  // загадываемое число
-        int[] Otgadka;  // Ваш ответ
-        int Counter = 1; //Счётчик ответов
-        int Score; //Количество очков сложности
-        int Level; //Сложность игры
-        int ScorePoint;//Счётчик очков
-
-        int Bulls = 0;
-        int Cows = 0;
-
-
-        private void buttonPopytka_Click(object sender, EventArgs e) //кнопка Угадать
-        {
-            Bulls = 0;
-            Cows = 0;
-
-
-
-            if (ScorePoint > 1)
-            {
-
-                
-                for (int it = 0; it < Level; it++)
-                {
-                    char c = InputOtgadka[it];
-                    if (char.IsDigit(c))
-                        Otgadka[it] = Convert.ToInt32(c - 48);
-                }
-
-                if (NumberCompare(StartNumber, Otgadka))
-                {
-                    rtbChat.AppendText("Вы угадали число " + InputOtgadka + " c " + Counter + " попытки!");
-                    buttonStart.Visible = true;
-                    buttonPopytka.Visible = false;
-                    return;
-                }
-
-                int[,] Analyze = new int[Level, 2];
-                // угаданная цифра Analyze[i,1], бык(Analyze[i,0]=2) корова(Analyze[i,0]=1)
-                // АНАЛИЗ. CountAnswer - число угаданных цифр
-                int CountAnswer = BullsAndСows(StartNumber, Otgadka, Analyze);
-
-                if (CountAnswer == 0)
-                {
-                    rtbChat.AppendText("Ход " + Counter + ". В Вашем числе " + InputOtgadka + " нет загаданных цифр..." + "\r\n");
-                    Counter++;
-                    rtbChat.ScrollToCaret();
-                }
-                else
-                {
-                    rtbChat.AppendText("Ход " + Counter + ". Число " + InputOtgadka + ". Быки = " + Bulls + ", Коровы = " + Cows + "\r\n");
-                    Counter++;
-                    rtbChat.ScrollToCaret();
-                }
-            }
-            else
-            {
-                rtbChat.AppendText("Вы не угадали загаданное компьютером число " + NumberToString() + " даже за " + Score + " предоставленных попыток..." + "\r\n");
-                buttonStart.Visible = true;
-                buttonPopytka.Visible = false;
-            }
-
-            ScorePoint -= 1;
-            labelScorePoint.Text = ScorePoint + " ";
-        }
-
-       
-
-        //берешь функцию
-        //создаёшь mock объект
-        //вызываешь эту функцию с  mock объектом и проверяешь каждую строчку
-
-        private void StartButton()
-        {
-            StartNumber = new int[Level];
-            Otgadka = new int[Level];
-            
-            GenSetN(Level, 10, StartNumber);
-            ScorePoint = Score;
-            
-            Counter = 1;
-        }
-
-        // Генератор множества Nq не повторяющихся целых чисел в диапазоне от 0 до Nm-1 (RVA)
-        static void GenSetN(int Nq, int Nm, int[] qN)
-        {
-            Random r = new Random();
-            int p, k = 0;
-            while (k < Nq)
-            {
-                p = r.Next(Nm);
-                bool b = true;
-                for (int i = 0; i < k; i++)
-                    if (p == qN[i])
-                    {
-                        b = false;
-                        break;
-                    }
-                if (b)
-                {
-                    qN[k] = p;
-                    k++;
-                }
-            }
-        }
-
-        // Сравнение заданного и введенного чисел
-        private int BullsAndСows(int[] stNum, int[] otg, int[,] anlz)
-        {
-            int k = 0;
-            for (int i = 0; i < Level; i++)
-            {
-                for (int j = 0; j < Level; j++)
-                {
-                    if (stNum[i] == otg[j])
-                    {
-                        anlz[k, 1] = stNum[i];
-                        if (i == j)
-                        {
-                            anlz[k, 0] = 2;
-                            Bulls += 1;
-                        }
-                        else
-                        {
-                            anlz[k, 0] = 1;
-                            Cows += 1;
-                        }
-                        k++;
-                    }
-                }
-            }
-            return k;
-        }
-
-        // Сравнение на чистое совпадение
-        private bool NumberCompare(int[] a, int[] b)
-        {
-            bool d = true;
-            for (int i = 0; i < Level; i++)
-            {
-                if (a[i] != b[i])
-                {
-                    d = false;
-                    break;
-                }
-            }
-            return d;
-        }
-
-        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void referenceToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Игра Быки-и-Коровы." + "\r\n" + "\r\n" +
-                "Автор - студент группы ЗП-81 Горелов Иван Александрович. " + "\r\n" + "\r\n" +
-                "Задание выбрано из списка тем, представленных для выполнения " + "\r\n" +
-                "в виде курсовой работы с использованием распределённой " + "\r\n" +
-                "системы контроля версий Github", "Справка");
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            panel2.Visible = false;
-        }
-
-        private void toolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            string Number = NumberToString();
-            toolStripMenuItem1.Text = Number;
-        }
-
-        private string NumberToString()
-        {
-            string Numb = "";
-            for (int i = 0; i < Level; i++)
-            {
-                Numb += StartNumber[i];
-            }
-            return Numb;
-        }
-
-        private void labelScorePoint_Click(object sender, EventArgs e)
-        {
-            ScorePoint = 2;
-            labelScorePoint.Text = "2";
-            Counter = Score-1;
-        }
-    }
-}
-*/
