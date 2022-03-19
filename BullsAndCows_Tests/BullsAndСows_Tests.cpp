41d420ac172f7d28dbffc0e2c68d56c7a01419da
GypoTalamus <vidzhet@inbox.ru>
2022-03-20 00:25:28 +0700

    Com 007 Rebrending func_Answer - failed...

---
+++ BullsAndCows_Tests/BullsAndСows_Tests.cpp
@@ -0,0 +1,73 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../MasterMind_v2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BullsAndСowsTests
+{
+	TEST_CLASS(BullsAndСowsTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int* StN = new int[4];
+			StN[0] = 1;
+			StN[1] = 2;
+			StN[2] = 3;
+			StN[3] = 4;
+			int* Otg = new int[4];
+			Otg[0] = 2;
+			Otg[1] = 1;
+			Otg[2] = 3;
+			Otg[3] = 4;
+
+			int CountAnswer = BullsAndСows(StN, Otg, 4);
+
+			Assert::AreEqual(4, CountAnswer);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			int* StN = new int[3];
+			StN[0] = 7;
+			StN[1] = 5;
+			StN[2] = 1;
+			int* Otg = new int[3];
+			Otg[0] = 7;
+			Otg[1] = 0;
+			Otg[2] = 5;
+
+			int CountAnswer = BullsAndСows(StN, Otg, 3);
+
+			Assert::AreEqual(2, CountAnswer);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			int* StN = new int[8];
+			StN[0] = 1;
+			StN[1] = 2;
+			StN[2] = 3;
+			StN[3] = 4;
+			StN[4] = 5;
+			StN[5] = 6;
+			StN[6] = 7;
+			StN[7] = 8;
+			int* Otg = new int[8];
+			Otg[0] = 2;
+			Otg[1] = 1;
+			Otg[2] = 5;
+			Otg[3] = 9;
+			Otg[4] = 4;
+			Otg[5] = 0;
+			Otg[6] = 7;
+			Otg[7] = 3;
+
+			int CountAnswer = BullsAndСows(StN, Otg, 8);
+
+			Assert::AreEqual(6, CountAnswer);
+		}
+	};
+}
--- MasterMind_v2.cpp
+++ MasterMind_v2.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
+#include <cctype>
 #include <ctime>
 
 using namespace std;
@@ -14,6 +15,7 @@ int Level; //Сложность игры
 
 int Bulls = 0; //Счётчик Быков
 int Cows = 0; //Счётчик Коров
+
 
 //Сравнение на чистое совпадение
 //Unit-test NumberCompare_Tests
@@ -56,13 +58,15 @@ static int BullsAndСows(int *StN, int *Otg, int lvl)
 
 
 //Ответ пользователя (строка в число), так же проверяется правильность ввода числа (только цифры, не менее и не более заданных параметрами Сложности игры)
-static bool Answer(int * Otg, int lvl)
+//static bool Answer(int * Otg, int lvl)
+static bool Answer(int* Otg, int lvl)
 {
     bool bb = true;
     Bulls = 0;
     Cows = 0;
     string ss;
     cout << "Введите Ваше " << lvl << "-значное число: ";
+
     while (bb)
     {
         cin >> ss;
@@ -70,52 +74,57 @@ static bool Answer(int * Otg, int lvl)
         for (int i = 0; i < lvl; i++)
         {
             char c = ss[i];
-            if ((isdigit(c)) && (ss.length() == lvl))
-            {
-                Otg[i] = c - '0';
-                bb = false;
+                if ((isdigit(c)) && (ss.length() == lvl))
+                {
+                    Otg[i] = c - '0';
+                    bb = false;
+                }
+                else
+                {
+                    cout << "Игрок, Вы ввели НЕ " << lvl << "-хзначное число! Будьте так любезны, введите " << lvl << "-значное число: " << endl;
+                    bb = true;
+                    break;
+                }
             }
-            else
-            {
-                cout << "Игрок, Вы ввели НЕ " << lvl << "-хзначное число! Будьте так любезны, введите " << lvl << "-значное число: " << endl;
-                break;
-            }
-        }
     }
     bb = true;
     return bb;
 }
 
 //Запрашиваем ввод числа-отгадки, проверяем на совпадение, подсчитываем количество попыток ответа
-void ProverkaOtveta()
+// НЕ всё можно протестировать Unit'ами
+void ProverkaOtveta(int lvl, int scor)
 {
-    while (Counter <= Score)
+    while (Counter <= scor)
     {
-        if (Answer(Otgadka, Level))  // ответ
-                if (NumberCompare(StartNumber, Otgadka, Level))
-                {
-                    cout << "Вы угадали число c " << Counter << " попытки!" << endl;
-                    return;
-                }
-        
-        int CountAnswer = BullsAndСows(StartNumber, Otgadka, Level);
+    if (Answer(Otgadka, lvl))  // ответ
+        if (NumberCompare(StartNumber, Otgadka, lvl))
+        {
+            cout << "Вы угадали число c " << Counter << " попытки!" << endl;
+            return;
+        }
+
+        int CountAnswer = BullsAndСows(StartNumber, Otgadka, lvl);
         if (CountAnswer == 0)
         {
-            cout << "Ход " << Counter << " . В Вашем числе нет загаданных цифр. Осталось " << Score-Counter << " попыток." << endl;
+            cout << "Ход " << Counter << " . В Вашем числе нет загаданных цифр. Осталось " << scor-Counter << " попыток." << endl;
         }
         else
         {
-            cout << "Ход " << Counter << ". Быки = " << Bulls << ", Коровы = " << Cows << ". Осталось " << Score-Counter << " попыток."<< endl;
+            cout << "Ход " << Counter << ". Быки = " << Bulls << ", Коровы = " << Cows << ". Осталось " << scor-Counter << " попыток."<< endl;
         }
         Counter++;
     }
 
     cout << "Вы не угадали загаданное компьютером число ";
-    for (int i = 0; i < Level; i++)
+    for (int i = 0; i < lvl; i++)
         cout << StartNumber[i];
-    cout << " даже за " << Score << " предоставленных попыток..." << endl;
+    cout << " даже за " << scor << " предоставленных попыток..." << endl;
 }
 
+
+//Проверяем цифры на неповторяемость в загаданном числе
+//Unit-test GenSetN_Tests
 static bool GenSetN(int step, int pTemp, int *StN)
 {
     bool nepovtor = true;
@@ -134,59 +143,61 @@ static bool GenSetN(int step, int pTemp, int *StN)
 
 
 //Инициализируем переменные для загаданного числа, отгадки, генерируем загаданное и запускаем получение и проверку ответа
-void Level_Settings()
+// НЕ всё можно протестировать Unit'ами
+void Level_Settings(int lvl, int scor)
 {
-    cout << "Компьютер загадал " << Level << "-значное число с НЕповторяющимися цифрами.У Вас есть " << Score << " попыток." << endl;
+    cout << "Компьютер загадал " << lvl << "-значное число с НЕповторяющимися цифрами.У Вас есть " << scor << " попыток." << endl;
     Counter = 1;
-    StartNumber = new int[Level];
-    Otgadka = new int[Level];
+    StartNumber = new int[lvl];
+    Otgadka = new int[lvl];
     //Рандомно генерируем множество StartNumber не повторяющихся целых чисел в диапазоне от 0 до 9 (10 цифр)
     int pTemp, step = 0;
-    while (step < Level)
+    while (step < lvl)
     {
         pTemp = rand() % 10;
         if (GenSetN(step, pTemp, StartNumber))
             step++;
     }
 
-    ProverkaOtveta();
+    ProverkaOtveta(lvl, scor);
     cout << "Игра завершена!" << endl << endl;
     system("pause");
 }
 
 
 //Переключатель-Меню
-void Menu(int ch)
+// Не всё можно протестировать Unit'ами
+void Menu(int ch, int lvl, int scor)
 {
     switch (ch)
     {
     case 1:
-        Level = 4;
-        Score = 10;
-        Level_Settings();
+        lvl = 4;
+        scor = 10;
+        Level_Settings(lvl, scor);
         break;
     case 2:
-        Level = 6;
-        Score = 20;
-        Level_Settings();
+        lvl = 6;
+        scor = 20;
+        Level_Settings(lvl, scor);
         break;
     case 3:
-        Level = 8;
-        Score = 40;
-        Level_Settings();
+        lvl = 8;
+        scor = 40;
+        Level_Settings(lvl, scor);
         break;
     case 4:
-        Level = 2;
-        Score = 10;
-        Level_Settings();
+        lvl = 2;
+        scor = 10;
+        Level_Settings(lvl, scor);
         break;
     case 5:
-        Level = 10;
-        Score = 80;
-        Level_Settings();
+        lvl = 10;
+        scor = 80;
+        Level_Settings(lvl, scor);
         break;
     case 0:
-        cout << "Прощай, Игрок! Надеюсь, мы ещё встретимся!.." << endl;
+        //cout << "Прощай, Игрок! Надеюсь, мы ещё встретимся!.." << endl;
         break;
     default:
         cout << "Такой команды нет, Игрок! Вернись и выбери заново!" << endl;
@@ -194,20 +205,21 @@ void Menu(int ch)
     }
 }
 
+
 //Основное тело программы
+// Не всё можно протестировать Unit'ами
 int main()
 {
     setlocale(LC_ALL, "Russian");
     srand(time(0));
     cout << endl << " Игра 'Быки и коровы'." << endl << " Привет, Игрок!" << endl << endl;
     int choice; //переменная для выбора пункта меню
-
     //Цикл вывода меню повторяется, пока игрок не введет 0 - выход из игры
     do
     {
         Level = 0;
         Score = 0;
-        cout <<  " = = = =  Меню  = = = = " << endl << endl <<
+        cout << " = = = =  Меню  = = = = " << endl << endl <<
             "1) Простая игра (4-значное число, 10 попыток)." << endl <<
             "2) Средняя игра (6-значное число, 20 попыток)." << endl <<
             "3) Сложная игра (8-значное число, 40 попыток)." << endl <<
@@ -216,6 +228,10 @@ int main()
             "0) Выход." << endl <<
             "Ваш выбор: ";
         cin >> choice;
-        Menu(choice);
+        Menu(choice, Level, Score);
     } while (choice != 0);
+
+    cout << "Прощай, Игрок! Надеюсь, мы ещё встретимся!.." << endl;
+    delete[] StartNumber;
+    delete[] Otgadka;
 }
